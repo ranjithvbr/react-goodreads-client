b48ebb2b54b5eae3ef2a9b8caa5d39f8aa722d8a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-27 13:55:37 +0000

    Deploy rathishvbr/react-goodreads-client to github.com/rathishvbr/react-goodreads-client.git:gh-pages

---
+++ precache-manifest.c1e2e0dafc46b8d8ab3279ee0d3bd89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3235275636e3cef484f4",
+    "url": "/react-goodreads-client/static/css/main.9edc9adf.chunk.css"
+  },
+  {
+    "revision": "3235275636e3cef484f4",
+    "url": "/react-goodreads-client/static/js/main.32352756.chunk.js"
+  },
+  {
+    "revision": "67dd4c78eb366f25d8f2",
+    "url": "/react-goodreads-client/static/css/1.2b4dc6fa.chunk.css"
+  },
+  {
+    "revision": "67dd4c78eb366f25d8f2",
+    "url": "/react-goodreads-client/static/js/1.67dd4c78.chunk.js"
+  },
+  {
+    "revision": "a5dd53808190e0de5a32",
+    "url": "/react-goodreads-client/static/js/runtime~main.a5dd5380.js"
+  },
+  {
+    "revision": "9829427d0f12ff1c921a32cb9ebdf5c6",
+    "url": "/react-goodreads-client/index.html"
+  }
+];
